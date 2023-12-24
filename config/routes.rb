a0acec89429a6afed879529429e7ff7e7a5960a3
shoya.fujisawa <6150fs@gmail.com>
2023-12-24 02:54:23 +0000

    ログイン中のユーザーの情報を取得するエンドポイントの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/users', to: 'user#create'
-  post '/users/login', to: 'user#login' 
+  post '/users/login', to: 'user#login'
+  get '/users/me', to: 'user#show' 
 end
